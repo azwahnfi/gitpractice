95cac450d7ce14d79852cebd8b75bac65bb6a367
azwahnfi <azwa_hanafiah@yahoo.com>
2021-10-24 16:51:13 +0800

    This is our fourth commit

--- app/src/main/java/com/example/gitpractice/MainActivity.java
+++ app/src/main/java/com/example/gitpractice/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         println("THIS IS CONTAINED IN MY SECOND COMMIT");
-        today is a good day
+        vgfhgbcvhhv
     }
 }
