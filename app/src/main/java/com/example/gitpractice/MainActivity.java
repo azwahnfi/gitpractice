38e45f78c0981f6b072e8dedd78b06885ab17700
azwahnfi <azwa_hanafiah@yahoo.com>
2021-10-24 16:40:58 +0800

    This is our third commit

--- app/src/main/java/com/example/gitpractice/MainActivity.java
+++ app/src/main/java/com/example/gitpractice/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         println("THIS IS CONTAINED IN MY SECOND COMMIT");
+        today is a good day
     }
 }
