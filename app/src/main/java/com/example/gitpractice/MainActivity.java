a92c7319255a6022e0ce5ab2dde33cf0f52e2596
azwahnfi <azwa_hanafiah@yahoo.com>
2021-10-29 12:29:42 +0800

    hello from your friend commit

--- app/src/main/java/com/example/gitpractice/MainActivity.java
+++ app/src/main/java/com/example/gitpractice/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         println("THIS IS CONTAINED IN MY SECOND COMMIT");
-        vgfhgbcvhhv
+        println("hello from your friend commit");
     }
 }
